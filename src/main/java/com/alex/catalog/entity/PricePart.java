6a322e1eb91f65804dd38aeb4b33c2500d7aecae
alex_man <alexmnsbs@gmail.com>
2023-06-17 09:14:48 +0300

    feat: add MachinePart

---
+++ src/main/java/com/alex/catalog/entity/PricePart.java
@@ -0,0 +1,29 @@
+package com.alex.catalog.entity;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * Класс создан в основном, чтобы показать вариант работы со встроенным классом.
+ * А так же задел на будущее, предполагалось, что тут будет еще одно-два поля
+ */
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Embeddable
+public class PricePart implements Serializable {
+
+    @Column(nullable = false)
+    private Double price;
+    
+}
